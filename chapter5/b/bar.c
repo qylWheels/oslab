27d85e21ee6fcc6d30dbd2e6b9b5b39f61cdae3a
qylWheels <command1748165360@126.com>
2023-11-20 15:42:49 +0800

    Finish part 2.1 in chapter5/b

--- chapter5/b/bar.c
+++ chapter5/b/bar.c
@@ -1,13 +1,22 @@
 void myprint(char* msg, int len);
 
-int choose(int a, int b)
-{
-	if(a >= b){
-		myprint("the 1st one\n", 13);
-	}
-	else{
-		myprint("the 2nd one\n", 13);
-	}
+// int choose(int a, int b)
+// {
+// 	if(a >= b){
+// 		myprint("the 1st one\n", 13);
+// 	}
+// 	else{
+// 		myprint("the 2nd one\n", 13);
+// 	}
 
-	return 0;
+// 	return 0;
+// }
+
+// print a string without a len hint
+void print(const char *str);
+
+void _main(int arg)
+{
+	char *msg = "hello world!\n";
+	print(msg);
 }
--- chapter5/b/foo.asm
+++ chapter5/b/foo.asm
@@ -9,22 +9,29 @@
 ; $
 
 extern choose	; int choose(int a, int b);
+extern _main
 
 [section .data]	; 数据在此
 
 num1st		dd	3
 num2nd		dd	4
+arg			dd	5
 
 [section .text]	; 代码在此
 
 global _start	; 我们必须导出 _start 这个入口，以便让链接器识别
 global myprint	; 导出这个函数为了让 bar.c 使用
+global print
 
 _start:
-	push	dword [num2nd]	; `.
-	push	dword [num1st]	;  |
-	call	choose		;  | choose(num1st, num2nd);
-	add	esp, 8		; /
+	; push	dword [num2nd]	; `.
+	; push	dword [num1st]	;  |
+	; call	choose		;  | choose(num1st, num2nd);
+	; add	esp, 8		; /
+
+	push dword [arg]
+	call _main
+	add esp, 4
 
 	mov	ebx, 0
 	mov	eax, 1		; sys_exit
@@ -39,3 +46,19 @@ myprint:
 	int	0x80		; 系统调用
 	ret
 	
+print:
+	mov esi, [esp + 4]	; base address of string
+write:
+	cmp byte [esi], 0
+	je end
+
+	mov edx, 1
+	mov ecx, esi
+	mov ebx, 1
+	mov eax, 4
+	int 0x80
+
+	add esi, 1
+	jmp write
+end:
+	ret
